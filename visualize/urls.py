18c99e0d13fc721e1048770ce40f8a580065cd82
renhonglei <renhongleiz@126.com>
2018-01-23 11:36:08 +0800

    init site

---
+++ visualize/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import patterns, include, url
+from django.contrib.auth import views as auth_views
+
+urlpatterns = patterns('visualize.views',
+ url(r'^index$','index',name='index'),
+)
