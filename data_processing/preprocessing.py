7908a931c4865e71f6dcc5ffdbfe63eef1cfed8c
renhonglei <renhongleiz@126.com>
2018-01-24 11:01:13 +0800

    0124

--- data_processing/preprocessing.py
+++ data_processing/preprocessing.py
@@ -5,8 +5,6 @@ def label_holiday(geo_points_list):
     #pass
     return holiday_labels
 
-
-
 if __name__ == "__main__":
     accident_fp =  os.path.join(base.origin_dir, "accident_loc.tsv")
     geo_points_list = base.read_origin_data_into_geo_point_list(accident_fp)
