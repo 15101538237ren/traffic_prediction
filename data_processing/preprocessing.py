fec2e7310431829df4cd137b3593e61a24a6c711
renhonglei <renhongleiz@126.com>
2018-01-24 11:27:39 +0800

    0124

--- data_processing/preprocessing.py
+++ data_processing/preprocessing.py
@@ -30,5 +30,5 @@ def label_region(geo_points_list):
 if __name__ == "__main__":
     accident_fp =  os.path.join(base.origin_dir, "accident_loc.tsv")
     geo_points_list = base.read_origin_data_into_geo_point_list(accident_fp)
-    label_holiday(geo_points_list)
+    # label_holiday(geo_points_list)
     label_region(geo_points_list)
